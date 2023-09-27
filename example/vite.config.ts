44bb8c3f17700b265e7ceb00e3f5b95f9c9eb2b0
Dimitri Lilov <dev.dmtrllv@gmail.com>
2023-09-27 18:47:49 +0200

    fix examples

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
 	plugins: [
+		topLevelAwait(),
 		react({
 			babel: {
 				parserOpts: {
