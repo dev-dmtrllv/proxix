72d01e9977a5fb7b43f1a0a3f185699c9b39c91e
Dimitri Lilov <dev.dmtrllv@gmail.com>
2023-10-04 15:10:12 +0200

    fix for jsdocs

--- lib/esm/State.d.ts
+++ lib/esm/State.d.ts
@@ -35,20 +35,85 @@ type AsyncHandlers<T> = {
     readonly reset: (resolver?: AsyncResolver<T>, prefetch?: boolean) => Promise<void>;
     readonly cancel: (prefetch?: boolean) => void;
 };
+/**
+ * @param state A state object which will automatically update all components that uses it.
+ * @returns The state object.
+ */
 export declare const create: <T extends {}>(state: T) => T;
+/**
+ * @param resolver An async function that will be resolved.
+ * @returns An `AsyncState` wrapped around the data or error returned by the async function.
+ */
 export declare const createAsync: <T>(resolver: AsyncResolver<T>) => AsyncState<T>;
+/**
+ * A class decorator to create a singleton based state object.
+ */
 export declare const global: <T extends new (...args: any[]) => any>(Class: T) => T;
+/**
+ * A decorator which enables updating components on property change.
+ */
 export declare const observable: PropertyDecorator;
+/**
+ * @summary A react hook to update the component on a state change.
+ * @param StateClass A state class.
+ * @param args The arguments which will be passed to the state class constructor.
+ */
 export declare function use<T, Args extends any[]>(StateClass: new (...args: Args) => T, ...args: Args): T;
+/**
+ * @summary A react hook to update the component on a state change.
+ * @param state A state object.
+ */
 export declare function use<T extends {}>(state: T): T;
+/**
+ * @param StateClass A state class.
+ * @param observer The observe callback which will be called when the state will change.
+ */
 export declare function observe<T extends {}, Args extends any[]>(StateClass: new (...args: Args) => T, observer: ObserveCallback<T>): Revoker;
+/**
+ * @param state A state object.
+ * @param observer The observe callback which will be called when the state will change.
+ */
 export declare function observe<T extends {}>(state: T, observer: ObserveCallback<T>): Revoker;
+/**
+ * @summary When the interceptor returns false, then the state change will be canceled.
+ * @param StateClass A state class.
+ * @param interceptor The intercept callback which will be called when the state will change.
+ */
 export declare function intercept<T extends {}, Args extends any[]>(StateClass: new (...args: Args) => T, interceptor: InterceptCallback<T>): Revoker;
+/**
+ * @summary When the interceptor returns false, then the state change will be canceled.
+ * @param state A state object.
+ * @param interceptor The intercept callback which will be called when the state will change.
+ */
 export declare function intercept<T extends {}>(state: T, interceptor: InterceptCallback<T>): Revoker;
+/**
+ * @param name The name which will be used to store and retreive the state from the localStorage.
+ * @param state The state object.
+ * @returns The state object.
+ */
 export declare const createPersistent: <T extends {}>(name: string, state: T) => T;
+/**
+ * Clears all the persistent states.
+ */
 export declare const clearPersistent: () => void;
+/**
+ * @param state The state object to observe.
+ * @param observer The observe callback.
+ */
 export declare const useObserve: <T extends {}>(state: T, observer: ObserveCallback<T>) => void;
+/**
+ * @param state The state object to reset.
+ */
 export declare const reset: <T extends {}>(state: T) => void;
+/**
+ * @param name The name which will be used to store and retreive the state from the localStorage.
+ * @param resolver An async function that will be resolved.
+ * @returns An `AsyncState` wrapped around the data or error returned by the async function.
+ */
 export declare const createAsyncPersistent: <T>(name: string, resolver: AsyncResolver<T>) => AsyncState<T>;
+/**
+ * @param state The global state class.
+ * @returns The instance of the global state class.
+ */
 export declare const getGlobal: <T extends {}>(state: new (...args: any) => T) => T;
 export {};
